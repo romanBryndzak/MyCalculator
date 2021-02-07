4fb1ca70b3f9e17ac70572834561a049e462a46e
Roman <romabruzak@gmail.com>
2021-02-07 14:22:00 +0200

    Added function lengthActionThree, startOperation, endOperation, lengthActionFive and code refactoring.

--- calculator.js
+++ calculator.js
@@ -19,18 +19,19 @@ buttons = [
     buttonC = document.getElementById('C'),
     buttonPercent = document.getElementById('percent'),
     buttonChange = document.getElementById('btnChange'),
-    output= document.getElementById('output'),
+    output = document.getElementById('output'),
     output1 = document.getElementById('output1'),
     output2 = document.getElementById('output2'),
 ];
 
 let a;
 let b;
-let action = [];
+let action;
+let arrayAction = [];
+let result;
 
 // функція приймає значення кнопки або ключ клавіші
 function actionOnClick(val) {
-
 
     function cleanOutput() {
         output.textContent = ''
@@ -44,6 +45,20 @@ function actionOnClick(val) {
         output2.textContent = ''
     }
 
+    function lengthActionThree() {
+        a = arrayAction[0];
+        action = arrayAction[1];
+        b = arrayAction[2];
+    }
+
+    function lengthActionFive() {
+        lengthActionThree()
+        startOperation()
+        a = result;
+        action = arrayAction[3];
+        b = arrayAction[4];
+    }
+
     function deleteOneSymbol(out) {
         let nu = out.textContent
         out.textContent = ''
@@ -54,38 +69,58 @@ function actionOnClick(val) {
         })
     }
 
+    function endOperation() {
+        output.textContent += ` = `;
+        output.textContent += result;
+        a = result;
+        output1.textContent += output.textContent;
+        output2.textContent += output1.textContent;
+        output2.textContent += '; ';
+        cleanOutput();
+        cleanOutput1();
+        arrayAction = [];
+        action = undefined;
+    }
+
     if (val === '=' || val === 'Enter') {
-        b = +output.textContent;
-        let result;
-        switch (action) {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case '*':
-                result = a * b;
-                break;
-            case '/':
-                result = a / b;
-                break;
-            default:
-                result = '';
-                break;
+        function startOperation() {
+            switch (action) {
+                case '*':
+                    result = a * b;
+                    break;
+                case '/':
+                    result = a / b;
+                    break;
+                case '+':
+                    result = a + b;
+                    break;
+                case '-':
+                    result = a - b;
+                    break;
+                default:
+                    result = '';
+                    break;
+            }
+        }
+
+        if (output1.textContent !== '') {
+            let number = +output.textContent;
+            arrayAction.push(number);
+        } else if (result === '') {
+            output.textContent = '';
+        }
+
+        if (arrayAction.length === 3) {
+            lengthActionThree()
+            startOperation()
+            endOperation()
+        } else if (arrayAction.length === 5) {
+            lengthActionFive()
+            startOperation()
+            endOperation()
         }
         if (result === '') {
             output.textContent += ``;
-        } else {
-            output.textContent += ` = `;
-            output.textContent += result;
-            a = result;
-            output1.textContent += output.textContent;
-            output2.textContent += output1.textContent;
-            output2.textContent += '; ';
-            cleanOutput();
-            cleanOutput1();
-            action = []
         }
 
         // якщо натиснуто символ 'С' або Del
@@ -93,20 +128,24 @@ function actionOnClick(val) {
         cleanOutput();
         cleanOutput1();
         cleanOutput2();
+        arrayAction = [];
 
         // якщо натиснуто символ 'СЕ' або Backspace
     } else if (val === 'CE' || val === 'Backspace') {
         if (output.textContent !== '') {
-           deleteOneSymbol(output)
+            deleteOneSymbol(output)
             a = output.textContent;
         } else if (output1.textContent !== '') {
             deleteOneSymbol(output1)
+            arrayAction.pop()
         }
         // якщо значення val (те яке очікуємо) рівне +,-,*,/.
     } else if (val === '+' || val === '-'
         || val === '*' || val === '/') {
-        action = val;
-        output.textContent += ` ${val} `;
+        let number = +output.textContent;
+        arrayAction.push(number);
+        arrayAction.push(val);
+        output.textContent += `${val}`;
         output1.textContent += output.textContent;
         cleanOutput();
 
@@ -122,7 +161,7 @@ function actionOnClick(val) {
         // якщо жодна з умова не виконана, записуємо значення val в поле
     } else {
         output.textContent += val;
-        a = +output.textContent;
+
     }
 }
 
@@ -135,7 +174,6 @@ function onClickValueButton(eventObject) {
 
 function keyupValueButton(event) {
     let valueCurrentElement = event.key;
-    console.log(valueCurrentElement)
     actionOnClick(valueCurrentElement)
 }
 
@@ -147,90 +185,3 @@ function onClickButtonAddEventListener(i) {
 for (let i = 0; i < buttons.length; i++) {
     onClickButtonAddEventListener(i)
 }
-
-
-// let arrayButtonsListener = [
-//     button1.addEventListener('click', onNumber1),
-//     button2.addEventListener('click', onNumber2),
-//     button3.addEventListener('click', onNumber3),
-//     button4.addEventListener('click', onNumber4),
-//     button5.addEventListener('click', onNumber5),
-//     button6.addEventListener('click', onNumber6),
-//     button7.addEventListener('click', onNumber7),
-//     button8.addEventListener('click', onNumber8),
-//     button9.addEventListener('click', onNumber9),
-//     button0.addEventListener('click', onNumber0),
-//     buttonPoint.addEventListener('click', onPoint),
-//     buttonDivide.addEventListener('click', onDivide),
-//     buttonMultiply.addEventListener('click', onMultiply),
-//     buttonMinus.addEventListener('click', onMinus),
-//     buttonPlus.addEventListener('click', onPlus),
-//     buttonSum.addEventListener('click', onSum),
-//     buttonCE.addEventListener('click', onCE),
-//     buttonC.addEventListener('click', onC),
-//     buttonPercent.addEventListener('click', onPercent),
-//     buttonChange.addEventListener('click', onChange),
-// ]
-
-// let onButton = [
-//     onNumber1 = () => {
-//         actionOnClick('1');
-//     },
-//     onNumber2 = () => {
-//         actionOnClick('2');
-//     },
-//     onNumber3 = () => {
-//         actionOnClick('3');
-//     },
-//     onNumber4 = () => {
-//         actionOnClick('4');
-//     },
-//     onNumber5 = () => {
-//         actionOnClick('5');
-//     },
-//     onNumber6 = () => {
-//         actionOnClick('6');
-//     },
-//     onNumber7 = () => {
-//         actionOnClick('7');
-//     },
-//     onNumber8 = () => {
-//         actionOnClick('8');
-//     },
-//     onNumber9 = () => {
-//         actionOnClick('9');
-//     },
-//     onNumber0 = () => {
-//         actionOnClick('0');
-//     },
-//     onPoint = () => {
-//         actionOnClick('.')
-//     },
-//     onDivide = () => {
-//         actionOnClick('/')
-//     },
-//     onMultiply = () => {
-//         actionOnClick('*')
-//     },
-//     onMinus = () => {
-//         actionOnClick('-')
-//     },
-//     onPlus = () => {
-//         actionOnClick('+')
-//     },
-//     onSum = () => {
-//         actionOnClick('=')
-//     },
-//     onCE = () => {
-//         actionOnClick('CE')
-//     },
-//     onC = () => {
-//         actionOnClick('C')
-//     },
-//     onPercent = () => {
-//         actionOnClick('%')
-//     },
-//     onChange = () => {
-//         actionOnClick('-/+')
-//     },
-// ]
